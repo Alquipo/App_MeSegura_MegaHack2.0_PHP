e9bbc4ea4176113dd9e2d93ada5d2eb80950fee1
vagrant <vagrant@homestead>
2020-05-02 03:45:59 +0000

    avancando com implementacao de home faltando acertar alinhamento

---
+++ resources/views/layouts/app2.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MESegura</title>
+
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="{{asset('css/custom.css')}}"></link>
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</head>
+<body class="">
+    <div class="container">
+        <br>
+        <div class="col-md-4 offset-md-4 col-sm-12">
+            @yield('content')
+            <footer>
+                @include('layouts.menu-app')
+            </footer>
+        </div>    
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/layouts/menu-app.blade.php
@@ -0,0 +1,46 @@
+<div class="menu-app">
+	<div class="col-sm-12 col-md-12">
+		<div class="row">
+			<div class="col-3">
+				<div class="row row justify-content-cente text-center">
+					<div class="col-12">
+						<i class="fa fa-money fa-menu-app" aria-hidden="true"></i>
+					</div>
+					<div class="col-12" style="padding: 0px;">
+					<span class="label-menu-app">Carteira</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<div class="row row justify-content-cente text-center">
+					<div class="col-12">
+						<i class="fa fa-bullseye fa-menu-app" aria-hidden="true"></i>
+					</div>
+					<div class="col-12" style="padding: 0px;">
+					<span class="label-menu-app">Objetivo</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<div class="row row justify-content-cente text-center">
+					<div class="col-12">
+						<i class="fa fa-sticky-note-o fa-menu-app" aria-hidden="true"></i>
+					</div>
+					<div class="col-12" style="padding: 0px;">
+					<span class="label-menu-app">Recomendações</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-3">
+				<div class="row row justify-content-cente text-center">
+					<div class="col-12">
+						<i class="fa fa-handshake-o fa-menu-app" aria-hidden="true"></i>
+					</div>
+					<div class="col-12" style="padding: 0px;">
+					<span class="label-menu-app">Parceiros</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
