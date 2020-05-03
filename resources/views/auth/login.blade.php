4eca83d760cb0a75667aaa2324d6e4a6f12b304a
vagrant <vagrant@homestead>
2020-05-03 14:40:27 +0000

    apagando lixo do login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,60 +28,6 @@
 
 </head>
 <body class="hold-transition login-page">
-{{-- <div class="login-box">
-    <div class="login-logo">
-        <a href="{{ url('/home') }}"><b>ME</b>Segura</a>
-    </div>
-
-    <!-- /.login-logo -->
-    <div class="login-box-body">
-        <p class="login-box-msg">Logue para iniciar sua sessão</p>
-
-        <form method="post" action="{{ url('/login') }}">
-            @csrf
-
-            <div class="form-group has-feedback {{ $errors->has('email') ? ' has-error' : '' }}">
-                <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
-                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                @if ($errors->has('email'))
-                    <span class="help-block">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </span>
-                @endif
-            </div>
-
-            <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
-                <input type="password" class="form-control" placeholder="Senha" name="password">
-                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                @if ($errors->has('password'))
-                    <span class="help-block">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </span>
-                @endif
-
-            </div>
-            <div class="row">
-                <div class="col-xs-8">
-                    <div class="checkbox icheck">
-                        <label>
-                            <input type="checkbox" name="remember"> Lembrar-me
-                        </label>
-                    </div>
-                </div>
-                <!-- /.col -->
-                <div class="col-xs-4">
-                    <button type="submit" class="btn btn-primary btn-block btn-flat">Logar</button>
-                </div>
-                <!-- /.col -->
-            </div>
-        </form>
-
-        <a href="{{ url('/password/reset') }}">Esqueci minha senha</a><br>
-        <a href="{{ url('/register') }}" class="text-center">Registrar novo usuário</a>
-
-    </div>
-    <!-- /.login-box-body -->
-</div> --}}
 <div class="container">
     <br>
     <div class="col-md-4 offset-md-4 col-sm-12">
